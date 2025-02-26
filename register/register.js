01de3cc95c196d53f0639f8588b50d95809f91ce
Wolphren <dav20084@byui.edu>
2025-02-26 12:03:29 -0700

    W08 assignments

--- register/register.js
+++ register/register.js
@@ -56,7 +56,7 @@ function totalFees()
   const feeElements = [...document.querySelectorAll("[id^=fee]")];
   let total = 0;
   for (const element of feeElements) 
-    {
+  {
     total += Number(element.value) || 0;
   }
   return total;
@@ -85,24 +85,25 @@ document.addEventListener('DOMContentLoaded', function()
         console.error("Could not find add button element!");
       }
       if (form) 
+      {
+      form.addEventListener('submit', function(event) 
+      {
+        event.preventDefault();
+        const adultName = document.getElementById('adult_name').value;
+        const fees = totalFees();
+        const info = 
         {
-        form.addEventListener('submit', function(event) 
-        {
-          event.preventDefault();
-          const adultName = document.getElementById('adult_name').value;
-          const fees = totalFees();
-          const info = 
-          {
-            name: adultName || "Customer",
-            participants: participantCount,
-            fees: fees
-          };
+          name: adultName || "Customer",
+          participants: participantCount,
+          fees: fees
+        };
 
-          form.style.display = 'none';
-          summarySection.innerHTML = successTemplate(info);
-          summarySection.style.display = 'block';
-        });
-      } else 
+        form.style.display = 'none';
+        summarySection.innerHTML = successTemplate(info);
+        summarySection.style.display = 'block';
+      });
+      } 
+      else 
       {
         console.error("Could not find form element!");
       }
